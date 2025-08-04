19eddcf3685cbc1c46154e456fa6aac360abe5a5
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-08-04 02:52:25 +0100

    setting up an server with express and socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-import { routes } from "./routes/routes.js";
+import express from "express";
+import { Server } from "socket.io";
+import { config } from "dotenv";
+import { createServer } from "http";
 
-let message: string = "Hello World";
-console.log(message);
-routes(" This is the router");
+config({ path: ".env" });
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production" ? false : ["http://localhost:3000"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("message", (data) => {
+    console.log(data);
+  });
+});
+
+httpServer.listen(process.env.PORT, () => {
+  console.log(`Listening on Port ${process.env.PORT}`);
+  console.log(`Running in ${process.env.NODE_ENV} mode`);
+});
