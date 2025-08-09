f33edb64b4e652aa266ae6472e95af3c05a32c7b
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-08-09 01:59:20 +0100

    use Express server instead of http server.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import { Server } from "socket.io";
 import { config } from "dotenv";
-import { createServer } from "http";
 
 config({ path: ".env" });
 
 const app = express();
-const httpServer = createServer(app);
+const expressServer = app.listen(process.env.PORT, () => {
+  console.log(`Listening on Port ${process.env.PORT}`);
+  console.log(`Running in ${process.env.NODE_ENV} mode`);
+});
 
-const io = new Server(httpServer, {
+const io = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === "production" ? false : ["http://localhost:3000"],
@@ -21,9 +23,4 @@ io.on("connection", (socket) => {
   socket.on("message", (data) => {
     console.log(data);
   });
-});
-
-httpServer.listen(process.env.PORT, () => {
-  console.log(`Listening on Port ${process.env.PORT}`);
-  console.log(`Running in ${process.env.NODE_ENV} mode`);
 });
