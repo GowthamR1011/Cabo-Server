c55bdae33277ed98cef6ae5089415c762ae2282a
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-08-12 23:57:24 +0100

    moved socket functionality to a socket subfolder.
    Cleaned up the code in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,10 @@
-import express from "express";
-import { Server } from "socket.io";
 import { config } from "dotenv";
 
 config({ path: ".env" });
 
-const app = express();
-const expressServer = app.listen(process.env.PORT, () => {
+import { app, server } from "./socket/socket.js";
+
+server.listen(process.env.PORT, () => {
   console.log(`Listening on Port ${process.env.PORT}`);
   console.log(`Running in ${process.env.NODE_ENV} mode`);
-});
-
-const io = new Server(expressServer, {
-  cors: {
-    origin:
-      process.env.NODE_ENV === "production" ? false : ["http://localhost:3000"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(socket.id);
-
-  socket.on("message", (data) => {
-    console.log(data);
-    socket.emit("Welcome to the Server");
-  });
-
-  socket.on("disconnect", (data) => {
-    console.log(`${socket.id} disconnected`);
-  });
 });
---
+++ src/socket/socket.ts
@@ -0,0 +1,30 @@
+import { Server } from "socket.io";
+
+import express from "express";
+import { createServer } from "http";
+
+const app = express();
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production" ? false : ["http://localhost:3000"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.emit("message", "Response");
+  });
+
+  socket.on("disconnect", (data) => {
+    console.log(`${socket.id} disconnected`);
+  });
+});
+
+export { app, server, io };
